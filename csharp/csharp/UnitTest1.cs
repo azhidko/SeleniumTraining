104f98dfb3c7958988c28a7a9e103add1669fafc
Zhidko Andriy <azhidko@sdl.com>
2016-11-16 18:27:59 +0200

    Task 1: Prepare infrastructure

---
+++ csharp/csharp/UnitTest1.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Support.UI;
+using NUnit.Framework;
+using OpenQA.Selenium.Firefox;
+
+namespace csharp
+{
+    [TestClass]
+    public class UnitTest1
+
+    {
+        private IWebDriver driver;
+        private WebDriverWait wait;
+
+        [SetUp]
+        public void start()
+        {
+            driver = new FirefoxDriver();
+            wait = new WebDriverWait(driver, TimeSpan.FromSeconds(10));
+
+        }
+
+        [Test]
+
+        public void FirstTest()
+        {
+            driver.Url = "http://google.com";
+            driver.FindElement(By.Name("q")).SendKeys("webdriver");
+            driver.FindElement(By.Name("btnG")).Click();
+            wait.Until(ExpectedConditions.TitleIs("webdriver - Пошук Google"));
+        }
+
+        [TearDown]
+
+        public void stop()
+        {
+            driver.Quit();
+            driver = null;
+        }
+
+        
+    }
+}
